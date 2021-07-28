cfc8c237dc7e3a0109725904bd3528d2729cb91c
Nate Holland <natesholland@gmail.com>
2021-07-28 14:27:44 -0500

    fixing css and checkpoint

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 import Nav from './Nav';
 
 const Logo = styled.h1`
-  background: red;
   font-size: 4rem;
   margin-left: 2rem;
   position: relative;
   z-index: 2;
+  background: red;
   transform: skew(-7deg);
   a {
     color: white;
@@ -20,13 +20,15 @@ const Logo = styled.h1`
 const HeaderStyles = styled.header`
   .bar {
     border-bottom: 10px solid var(--black, black);
+    display: grid;
     grid-template-columns: auto 1fr;
     justify-content: space-between;
     align-items: center;
   }
+
   .sub-bar {
     display: grid;
-    grid-template-columns: 1ft auto;
+    grid-template-columns: 1fr auto;
     border-bottom: 1px solid var(--black, black);
   }
 `;
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,4 +1,14 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import PropTypes from 'prop-types';
+
 import Page from '../components/Page';
+
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
@@ -7,3 +17,8 @@ export default function MyApp({ Component, pageProps }) {
     </Page>
   );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.any,
+};
